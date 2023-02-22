8b0fbdc9af367ca801cd4b3930b24ea36e49de49
Horizon719 <milichovszky.sandor@gmail.com>
2023-02-22 13:30:44 +0100

    Push javítás(színes blokkok)

--- Equalizer.java
+++ Equalizer.java
@@ -1,14 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package oraimunka02.pkg22;
 
-/**
- *
- * @author Sanya
- */
-public class Equalizer {
+import java.util.Random;
 
+public class Equalizer {
+    public static void main(String[] args) {
+        kiir();
+    }
+
+    private static void kiir() {
+        Random rnd = new Random();
+        for (int i = 0; i < 5; i++) {
+            int mennyi = rnd.nextInt(7-3+1)+3;
+            String szoveg = "\u001B[45m" + " ";
+            String ki = "";
+            for (int j = 0; j < mennyi; j++) {
+                ki += szoveg;
+            }
+            System.out.println(ki);
+        }
+    }
 }
