e662b8b9160f371915348072d6c3e31210a9b5e4
Horizon719 <milichovszky.sandor@gmail.com>
2023-02-22 12:20:52 +0100

    kiírások

--- OraiMunka0222.java
+++ OraiMunka0222.java
@@ -3,24 +3,26 @@ package oraimunka02.pkg22;
 public class OraiMunka0222 {
 
     public static void main(String[] args) {
-        elso10();
+        int a = 4;
+        int b = 5;
+        kiir("Az elso tiz szam osszege: " + elso10SzamOsszege() + "\n");
+        kiir(a + "+" + b + "=" + osszead(a, b) + "\n");
     }
     
-    private static void elso10(){
+    private static int elso10SzamOsszege(){
         int osszeg = 0;
         for (int i = 0; i < 10; i++) {
             osszeg += i;
-            osszead(osszeg, i);
         }
-        kiir(osszeg);
-    }
-
-    private static void kiir(int osszeg){
-        System.out.println("Az elso 10 szam osszege: " + osszeg);
+        return osszeg;
     }
     
     private static int osszead(int a, int b){
-        return a += b;
+        return a + b;
+    }
+    
+    private static void kiir(String szoveg){
+        System.out.print(szoveg);
     }
     
 }
