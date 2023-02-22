e591c0c9b7fe4401b5b67dad0e41bc0d3c1d4c55
Horizon719 <milichovszky.sandor@gmail.com>
2023-02-22 13:49:57 +0100

    parameteres és nélüli equalizer

--- Equalizer.java
+++ Equalizer.java
@@ -4,19 +4,35 @@ import java.util.Random;
 
 public class Equalizer {
     public static void main(String[] args) {
-        kiir();
+        eq_5sorbol();
+    }
+    
+    private static void eq_5sorbol() {
+        eq();
+        eq(12);
+        eq();
+        eq(8);
+        eq();
     }
 
-    private static void kiir() {
+    private static void eq() {
         Random rnd = new Random();
-        for (int i = 0; i < 5; i++) {
-            int mennyi = rnd.nextInt(7-3+1)+3;
-            String szoveg = "\u001B[45m" + " ";
-            String ki = "";
-            for (int j = 0; j < mennyi; j++) {
-                ki += szoveg;
-            }
-            System.out.println(ki);
+        
+        int mennyi = rnd.nextInt(7-3+1)+3;
+        String szoveg = "\u001B[45m" + " ";
+        String ki = "";
+        for (int j = 0; j < mennyi; j++) {
+            ki += szoveg;
         }
+        System.out.println(ki);
+    }
+    
+    private static void eq(int mennyi) {
+        String szoveg = "\u001B[45m" + " ";
+        String ki = "";
+        for (int j = 0; j < mennyi; j++) {
+            ki += szoveg;
+        }
+        System.out.println(ki);
     }
 }
