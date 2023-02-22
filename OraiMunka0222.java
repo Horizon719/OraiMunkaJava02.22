b1a8c4b46330cb563d7a412b7a95801dd517d902
Horizon719 <milichovszky.sandor@gmail.com>
2023-02-22 12:51:41 +0100

    mainben csak 1 metodus

--- OraiMunka0222.java
+++ OraiMunka0222.java
@@ -5,8 +5,15 @@ public class OraiMunka0222 {
     public static void main(String[] args) {
         int a = 4;
         int b = 5;
+        int c = 6;
+        int d = 7;
+        int osszeg1 = osszead(a, b);
+        int osszeg2 = osszead(c, d);
+        int osszeg3 = osszead(osszeg1, c);
         kiir("Az elso tiz szam osszege: " + elso10SzamOsszege() + "\n");
         kiir(a + "+" + b + "=" + osszead(a, b) + "\n");
+        kiir(a + "+" + b + + c + d + "=" + osszead(osszeg1, osszeg2) + "\n");
+        kiir("Az a + b + c gyöke=" + Math.sqrt(osszeg3) + "\n");
     }
     
     private static int elso10SzamOsszege(){
